21d1b977c63ff7169d3e2342adbac74f75812c71
Gabriel Alves <gabrielgabriel352@gmail.com>
2024-11-06 08:39:25 -0300

    1.1

--- login.php
+++ login.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-    <?php
+<?php
         $db = new SQLite3('bd.sqlite');
         $db-> query('CREATE TABLE IF NOT EXISTS "usuarios"(
                     "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                    "nome" VARCHAR,
-                    "departamento" VARCHAR  )
+                    "nome" VARCHAR NOT NULL,
+                    "departamento" VARCHAR NOT NULL  )
                     ');
         $db-> query('CREATE TABLE IF NOT EXISTS "estoque" (
                     "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                    "produto" VARCHAR,
-                    "preco" INT,
+                    "produto" VARCHAR NOT NULL,
+                    "preco" INT NOT NULL,
                     "quantidade" INT)'
                     );
-    ?>
+                    ?>
+
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/style.css">
-    <title>Lanchonete</title>
+    <title>login - Lanchonete</title>
 </head>
 <body>
-                    
     
 </body>
 </html>
--- page/home.php
+++ page/home.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-br"></html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/style.css">
-    <title>login - Lanchonete</title>
+    <title>Lanchonete</title>
 </head>
 <body>
+                    
     
 </body>
 </html>
